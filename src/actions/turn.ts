68b86491aaf3ae72c070c4cbdee8245bf1051d73
demos-prog <demosne@gmail.com>
2024-10-20 14:15:29 +0300

    minor

--- src/actions/turn.ts
+++ src/actions/turn.ts
@@ -2,26 +2,26 @@ import { GameType } from "../entities/Game.type";
 import { PlayerType } from "../entities/Player.type";
 import { games, players } from "../gameData";
 
-export function turn(gameId: string | number, nextPlayer?: number | string) {
+export function turn(gameId: string | number, nextPlayerId?: number | string) {
   const game: GameType | undefined = games.get(gameId);
   if (!game) {
     console.error("Game not found");
     return;
   }
 
-  let playerTurn: number | string = ""; // ID of the player who will play
+  let playerTurnId: number | string = ""; // ID of the player who will play
 
   const randomNumber = Math.floor(Math.random() * 2);
   if (randomNumber === 0) {
-    playerTurn = game.players[0].indexPlayer;
+    playerTurnId = game.players[0].indexPlayer;
   } else {
-    playerTurn = game.players[1].indexPlayer;
+    playerTurnId = game.players[1].indexPlayer;
   }
 
   const response = {
     type: "turn",
     data: JSON.stringify({
-      currentPlayer: nextPlayer ? nextPlayer : playerTurn,
+      currentPlayer: nextPlayerId ? nextPlayerId : playerTurnId,
     }),
     id: 0,
   };
