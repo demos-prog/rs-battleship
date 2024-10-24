d66a453efdde477c9954a85f54e2d8b379d42422
demos-prog <demosne@gmail.com>
2024-10-24 08:26:56 +0300

    fix: adding existing user to room

--- src/actions/addUserToRoom.ts
+++ src/actions/addUserToRoom.ts
@@ -4,6 +4,8 @@ import { createGame } from "./createGame";
 import { updateRoom } from "./updateRoom";
 import { getUsersData } from "./getUsersData";
 import { rooms } from "../gameData";
+import { RoomType } from "../entities/Room.type";
+import { RoomUserType } from "../entities/RoomUser.type";
 
 export function addUserToRoom(ws: WebSocket, dto: AddUserToRoomDto) {
   let data = dto.data;
@@ -14,19 +16,26 @@ export function addUserToRoom(ws: WebSocket, dto: AddUserToRoomDto) {
   const user = getUsersData(ws);
   const indexRoom = data.indexRoom;
 
-  const room = rooms.get(indexRoom);
+  const room: RoomType = rooms.get(indexRoom);
   if (!room) {
     console.error("Room not found");
     return;
   }
 
-  room.roomUsers.push({
-    name: user?.name,
-    index: user?.index,
-  });
+  const isRoomUnAvalible = room.roomUsers.some(
+    (userInRoom: RoomUserType) => userInRoom.index === user?.index
+  );
+  if (user && !isRoomUnAvalible) {
+    room.roomUsers.push({
+      name: user?.name,
+      index: user?.index,
+    });
 
-  rooms.set(indexRoom, room);
-  console.log("Add user to room");
-  updateRoom();
-  createGame(indexRoom);
+    rooms.set(indexRoom, room);
+    console.log("Add user to room");
+    updateRoom();
+    createGame(indexRoom);
+  } else {
+    console.error("Room is unavalible");
+  }
 }
